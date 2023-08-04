cb7136e7b6d6c1015acc7b9f05bb6e6c50708835
Your Name <you@example.com>
2023-08-04 19:03:41 +0300

    fix

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"golang.org/x/exp/slices"
 	"os"
 	"os/exec"
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
-func main() {
+func pull() {
+	// Dirs to be ignored when pulling
 	pullignoreData, _ := os.ReadFile(".pullignore")
 	pullignore := strings.Split(string(pullignoreData), "\r\n")
+	configData, _ := os.ReadFile("config")
+	config := strings.Split(string(configData), "\r\n")
+	dst := ""
+
+	// Get seial number, as reported by adb devices
+	cmd := exec.Command("adb", "shell", "getprop", "ro.serialno")
+	device_id, _ := cmd.Output()
+
+	for _, v := range config {
+		option := strings.Split(v, "=")
+		key := option[0]
+		value := option[1]
+
+		// handle config options
+		if key == "DST" {
+			dst = value + "\\" + string(device_id)
+		}
+	}
 
 	ls := exec.Command("adb", "shell", "ls", "-d", "/sdcard/*")
 
@@ -18,25 +39,53 @@ func main() {
 
 	folders := strings.Split(string(stdout), "\r\n")
 
-	foldersToPull := ""
+	// exclude ignored folders
+	foldersToPull := make([]string, len(folders))
 	for _, v := range folders {
 		if slices.Contains(pullignore, strings.Trim(v, " ")) == false {
-			foldersToPull += v + " "
+			foldersToPull = append(foldersToPull, strings.Trim(v, " "))
 		}
 	}
 
-	fmt.Println(foldersToPull)
+	fmt.Println("Pulling these dirs: " + strings.Trim(strings.Join(foldersToPull, " "), " "))
+	fmt.Println(".....................")
+	fmt.Println("to...................")
+	fmt.Println(dst)
+	fmt.Println(".....................")
 
-	// cmd = exec.Command(app, arg0, arg1)
-	// stdout, err := cmd.Output()
+	// cmd cuz os.MkdirAll is too stupid (maybe it's only a Windows problem)
+	cmd = exec.Command("cmd", "/c", "mkdir", dst)
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
+	_, _ = cmd.Output()
 
-	// // Print the output
-	// fmt.Println(string(stdout))
-	// utput
-	// fmt.Println(string(stdout))
+	for _, v := range foldersToPull {
+
+		if v == "" {
+			continue
+		}
+
+		pull := exec.Command("adb", "pull", v, strings.ReplaceAll(dst, "\r\n", ""))
+
+		// print command with src and dst paths
+		fmt.Println(pull)
+
+		stdout, _ := pull.Output()
+
+		// print result
+		fmt.Println(string(stdout))
+	}
+}
+
+func main() {
+	modePtr := flag.String("mode", "", "a string")
+
+	flag.Parse()
+
+	fmt.Println("Running in mode: " + *modePtr)
+
+	if *modePtr == "pull" {
+		pull()
+	} else {
+		fmt.Println("no such mode, or no mode passed. try to run with '-mode pull'")
+	}
 }
