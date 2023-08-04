bbe8547c3a158fa738d4089560583f6a99fcac88
Your Name <you@example.com>
2023-08-04 17:57:49 +0300

    bbb

--- main.go
+++ main.go
@@ -2,41 +2,41 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"os/exec"
 	"golang.org/x/exp/slices"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-pullignoreData, _ := os.ReadFile(".pullignore")
-pullignore := strings.Split(string(pullignoreData), "\r\n")
+	pullignoreData, _ := os.ReadFile(".pullignore")
+	pullignore := strings.Split(string(pullignoreData), "\r\n")
 
-ls := exec.Command("adb", "shell", "ls", "-d", "/sdcard/*")
+	ls := exec.Command("adb", "shell", "ls", "-d", "/sdcard/*")
 
-stdout, _ := ls.Output()
+	stdout, _ := ls.Output()
 
-folders := strings.Split(string(stdout), "\r\n")
+	folders := strings.Split(string(stdout), "\r\n")
 
-foldersToPull := ""
-for _, v := range folders {
-	if (slices.Contains(pullignore, strings.Trim(v, " ")) == false) {
-		foldersToPull += v + " "
+	foldersToPull := ""
+	for _, v := range folders {
+		if slices.Contains(pullignore, strings.Trim(v, " ")) == false {
+			foldersToPull += v + " "
+		}
 	}
-}
 
-fmt.Println(foldersToPull)
+	fmt.Println(foldersToPull)
 
-// cmd = exec.Command(app, arg0, arg1)
-// stdout, err := cmd.Output()
+	// cmd = exec.Command(app, arg0, arg1)
+	// stdout, err := cmd.Output()
 
-// if err != nil {
-// 	fmt.Println(err.Error())
-// 	return
-// }
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
 
-// // Print the output
-// fmt.Println(string(stdout))
-// utput
-// fmt.Println(string(stdout))
+	// // Print the output
+	// fmt.Println(string(stdout))
+	// utput
+	// fmt.Println(string(stdout))
 }
